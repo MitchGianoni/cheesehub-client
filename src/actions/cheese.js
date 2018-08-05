529ccc1ebcc766bccaef38cb3c6a4eab1732ca58
MitchGianoni <mitchgianoni@yahoo.com>
2018-08-05 04:13:45 -0400

    added cheese actions

---
+++ src/actions/cheese.js
@@ -0,0 +1,34 @@
+export const FETCH_CHEESES_REQUEST = 'FETCH_CHEESES_REQUEST';
+export const fetchCheesesRequest = () => ({
+  type: FETCH_CHEESES_REQUEST
+});
+
+export const FETCH_CHEESES_SUCCESS = 'FETCH_CHEESES_SUCCESS';
+export const fetchCheesesSuccess = (cheeses) => ({
+  type: FETCH_CHEESES_SUCCESS,
+  cheeses
+});
+export const FETCH_CHEESES_ERROR = 'FETCH_CHEESES_ERROR';
+export const fetchCheesesError = (error) => ({
+  type: FETCH_CHEESES_ERROR,
+  error
+});
+
+export const FETCH_CHEESES = 'FETCH_CHEESES';
+export const fetchCheeses = () => dispatch => {
+  console.log('fetch cheeses');
+  dispatch(fetchCheesesRequest());
+  return fetch('localhost:8080/api/cheeses')
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.json();
+    })
+    .then(data => {
+      dispatch(fetchCheesesSuccess(data));
+    })
+    .catch(err => {
+      dispatch(fetchCheesesError(err));
+    });
+};
